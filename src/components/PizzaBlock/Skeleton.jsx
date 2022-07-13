c19a0906af965e46c94594d597b08ba90993a98c
bekhzodx64 <mr.bekzod2016@mail.ru>
2022-07-13 22:30:44 +0500

    added react router, not found page, react skeleton

---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,20 @@
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+	<ContentLoader
+		className='pizza-block'
+		speed={2}
+		width={280}
+		height={500}
+		viewBox='0 0 280 500'
+		backgroundColor='#f3f3f3'
+		foregroundColor='#ecebeb'>
+		<circle cx='140' cy='125' r='125' />
+		<rect x='0' y='275' rx='10' ry='10' width='280' height='32' />
+		<rect x='0' y='335' rx='10' ry='10' width='280' height='88' />
+		<rect x='0' y='447' rx='10' ry='10' width='95' height='30' />
+		<rect x='128' y='440' rx='25' ry='25' width='152' height='45' />
+	</ContentLoader>
+);
+
+export default Skeleton;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Sort from '../components/Sort';
+
+const Home = () => {
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		fetch('https://62cedd9e826a88972d04976a.mockapi.io/items')
+			.then((res) => res.json())
+			.then((arr) => {
+				setItems(arr);
+				setIsLoading(false);
+			});
+	}, []);
+
+	return (
+		<>
+			<div className='content__top'>
+				<Categories />
+				<Sort />
+			</div>
+			<h2 className='content__title'>Все пиццы</h2>
+			<div className='content__items'>
+				{isLoading
+					? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+					: items.map((item) => <PizzaBlock key={item.id} {...item} />)}
+			</div>
+		</>
+	);
+};
+
+export default Home;
